f4a8360d14711960e1db3876ebcd119485caa486
Li Jia <tiwb@msn.com>
2026-04-23 15:41:34 +0800

    chore: 版本号升级至 0.7.999

--- src/mutobj/__init__.py
+++ src/mutobj/__init__.py
@@ -7,7 +7,7 @@ mutobj - Mutable Object Declaration
 from mutobj.core import Declaration, Extension, impl, field, register_module_impls, unregister_module_impls, \
     discover_subclasses, get_registry_generation, resolve_class, extensions, extension_types, get_declaration_doc
 
-__version__ = "0.6.999"
+__version__ = "0.7.999"
 __all__ = ["Declaration", "Extension", "impl", "field", "register_module_impls", "unregister_module_impls",
            "discover_subclasses", "get_registry_generation", "resolve_class", "extensions", "extension_types",
            "get_declaration_doc"]
